eff95b86fa83cfbd14d702953c4715f3bfdc289e
nyllon12 <n8263033@gmail.com>
2025-09-16 13:46:47 +0530

    Updated main.ts

--- project/src/main.ts
+++ project/src/main.ts
@@ -99,7 +99,7 @@
 
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter, Routes } from '@angular/router';
-import { AppComponent } from './app/app.component';
+//import { AppComponent } from './app/app.component';
 
 import { HomeComponent } from './app/components/home/home.component';
 import { AboutComponent } from './app/components/about/about.component';
